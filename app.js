d108f1bd5bba2b936c0b1767a4a65b09a494f87e
Your Name <you@example.com>
2024-09-14 15:32:50 +0530

    add cors 1

--- app.js
+++ app.js
@@ -46,7 +46,7 @@ app.use(
 //   credentials: true,
 // };
 
-app.use(cors(corsOptions));
+app.use(cors(allowedOrigins));
 
 app.use(bodyParser.json({ limit: "200mb" }));
 app.use(bodyParser.urlencoded({ limit: "300mb", extended: true }));
